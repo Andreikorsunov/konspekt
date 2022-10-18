55476bd8b197d260d9f175301a8a4b1cd3365bb7
Andreikorsunov <77333208+Andreikorsunov@users.noreply.github.com>
2022-10-18 09:16:10 +0300

    opjat main files

---
+++ musoropjat/obj/Debug/net6.0/2wzcfu51.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+
+ALTER TABLE "Users" ADD "IsMarried" INTEGER NOT NULL DEFAULT 0;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20220921060134_IsMarriedToUserAdded', '6.0.9');
+
+COMMIT;
+
