ef4b634408f55e964c4f18cebd684eb1933bbadd
Andreikorsunov <77333208+Andreikorsunov@users.noreply.github.com>
2022-11-01 09:04:53 +0200

    Delete konspekt/obj directory

--- konspekt/obj/Debug/net6.0/2wzcfu51.sql
+++
@@ -1,9 +0,0 @@
-﻿BEGIN TRANSACTION;
-
-ALTER TABLE "Users" ADD "IsMarried" INTEGER NOT NULL DEFAULT 0;
-
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20220921060134_IsMarriedToUserAdded', '6.0.9');
-
-COMMIT;
-
